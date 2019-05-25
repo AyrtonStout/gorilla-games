6be987d340de31ce25954b00c228c0b89807fbd6
Ayrton Stout <ayrtonstout@outlook.com>
2019-05-25 12:55:45 -0700

    Fix emcc build script

--- games/blocks/emcc-build.sh
+++ games/blocks/emcc-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p dist
 FILES=$(ls src/*.cpp)
-ARGS="-O2 -std=c++11 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='[\"png\"]' -s MODULARIZE=1 --preload-file assets -o dist/blocks.js"
-echo BUILDING emcc ${FILES} ${ARGS}
-emcc ${FILES} ${ARGS}
+ARGS="-O2 -std=c++11 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s MODULARIZE=1 --preload-file assets -o dist/blocks.js"
+echo BUILDING emcc ${FILES} ${ARGS} -s SDL2_IMAGE_FORMATS='["png"]'
+emcc ${FILES} ${ARGS} -s SDL2_IMAGE_FORMATS='["png"]'
