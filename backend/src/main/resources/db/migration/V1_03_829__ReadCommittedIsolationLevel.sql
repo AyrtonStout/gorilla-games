b1f5b90d3dc9012de30e8a81c9de2a44b51702ce
AyrtonStout <AyrtonStout@outlook.com>
2019-08-07 22:54:30 -0600

    Remove root-access requiring mysql query because it didn't even matter anyway

--- backend/src/main/resources/db/migration/V1_03_829__ReadCommittedIsolationLevel.sql
+++
@@ -1,3 +0,0 @@
--- MySQL is apparently like the only DB that has a default isolation level of REPEATABLE READ
--- This screws up long polling because our transaction will never get new data
-SET GLOBAL TRANSACTION ISOLATION LEVEL READ COMMITTED;
